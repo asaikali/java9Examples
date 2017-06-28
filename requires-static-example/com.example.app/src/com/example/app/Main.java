183b4c5c87a6aa2f6be1000d437ed2417d0a1266
Adib Saikali <asaikali@gmail.com>
2017-06-28 16:32:52 -0400

    example with requires static

--- requires-static-example/com.example.app/src/com/example/app/Main.java
+++ requires-static-example/com.example.app/src/com/example/app/Main.java
@@ -1,7 +1,10 @@
 package com.example.app;
 
+import com.example.foo.api.Foo;
+
 public class Main {
     public static void main(String[] args) {
-
+        Foo foo = new Foo();
+        foo.doSomething();
     }
 }
