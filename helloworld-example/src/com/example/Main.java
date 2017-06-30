e48f30a1ee3a349f3a07cc5f296b125e0c5857bf
Adib Saikali <asaikali@gmail.com>
2017-06-30 12:38:33 -0400

    hello world example

---
+++ helloworld-example/src/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger("Main");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ helloworld-example/src/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by asaikali on 6/30/17.
+ */
+module helloworld.example {
+    requires java.logging;
+}
